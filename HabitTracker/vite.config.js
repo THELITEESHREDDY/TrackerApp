2e7f7842d36595ee5b1359de498d862396f5de3b
Liteesh Reddy <liteeshreddypotlapalli.3605@gmail.com>
2026-05-05 19:33:18 +0530

    made it into pwa

--- HabitTracker/vite.config.js
+++ HabitTracker/vite.config.js
@@ -1,11 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+     VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Consistent: Habit Builder and tracker',
+        short_name: 'Consitent',
+        description: 'THis actually help us to build and track our new habits every day',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose:'any'
+          },
+          {
+            src: 'manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose:'any'
+          },
+          {
+            src: 'manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose:'maskable'
+          }
+        ]
+      }
+    })
   ],
 })
